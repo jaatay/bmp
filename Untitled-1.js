118e97f3c20608384c49e55774ed778a9fbf45f7
Jesse Atay <jesse.atay@gmail.com>
2019-02-25 03:30:41 -0800

    transform updates

--- Untitled-1.js
+++ Untitled-1.js
@@ -48,8 +48,67 @@ const writeNewFile = inputFile => {
     });
 }
 
+// test transformations
+const testTransform = (colorArray) => {
+    for(let i = 0; i < colorArray.length; i++){
+        if(colorArray[i] === 0){
+            colorArray[i] = 255;
+        }
+     }
+}
+
+const allTransform = (colorArray) => {
+    for(let i = 0; i < colorArray.length; i++){
+        colorArray[i] = 0;
+    }
+}
+
+const allTransformWhite = (colorArray) => {
+    for(let i = 0; i < colorArray.length; i++){
+        colorArray[i] = 255;
+     }
+}
+
+const reverseTransform = (colorArray) => {
+    let reverseCounter = colorArray.length - 1;
+    for(let i = 0; i < colorArray.length; i++){
+        colorArray[i] = reverseCounter;
+        reverseCounter --;
+    }
+}
+
+const neonTransform = (colorArray) => {
+
+    for(let i = 0; i < colorArray.length; i+=3){
+        colorArray[i] = 0;
+        colorArray[i + 1] = 191;
+        colorArray[i + 2] = 255;
+    }
+}
+
+const neonTransform2 = (colorArray) => {
+
+    for(let i = 0; i < colorArray.length; i+=3){
+        colorArray[i] = 255;
+        colorArray[i + 1] = 0;
+        colorArray[i + 2] = 191;
+        colorArray[i + 3] = 0;
+    }
+}
+
+//pixel array transform
+
+const pixelTransform = (pixelArray) => {
+    let reverseCounter = pixelArray.length - 1;
+    for(let i = 0; i < pixelArray.length; i++){
+        pixelArray[i] = reverseCounter;
+        reverseCounter --;
+    }
+}
+
+
 //file read
-const readFile = file => {
+const readFile = (file, transformation) => {
 
     fs.readFile(file, function(err, data){
         if(err){
@@ -57,28 +116,26 @@ const readFile = file => {
         }
 
         let newBitmap = new Bitmap(data);
-       // console.log(newBitmap);
-
     
      newBitmap.parse(data);
 
 
-        // console.log(newBitmap);
-       // console.log(newBitmap.offset);
-       //console.log(newBitmap.colorArray);
+       console.log(newBitmap.offset);
+      // console.log(newBitmap.colorArray);
+      console.log(newBitmap.pixelArray);
 
-       let colorArrayJson = convertToJSON(newBitmap.colorArray);
 
-       console.log(colorArrayJson.data);
+      
+        //color array
+       let colorArray = data.slice(54, newBitmap.offset);
+       console.log(colorArray);
 
-        for(let i = 0; i < colorArrayJson.data; i++){
-           if(colorArrayJson.data[i] == 0){
-               colorArrayJson.data[i] = 255;
-           }
-        }
+       //pixel array
+       let pixelArray = data.slice(newBitmap.pixelArray, 14068);
+       console.log(pixelArray);
 
-        console.log(colorArrayJson.data);
-        newBitmap.colorArray = colorArrayJson;
+
+       transformation(colorArray);
         writeNewFile(data);
 
     
@@ -88,4 +145,5 @@ const readFile = file => {
 
 }
 
-readFile('./assets/baldy.bmp');
+readFile('./assets/baldy.bmp', neonTransform2);
+
